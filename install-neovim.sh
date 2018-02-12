379ff796c2fb9d1d84e475a6d6b209d82f4f9125
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2018-02-12 15:55:16 +0100

    installfile

---
+++ install-neovim.sh
@@ -0,0 +1,10 @@
+echo "Installing deps"
+sudo pacman -S git neovim clang python-neovim python2-neovim cmake gcc
+
+echo "Clone git repo"
+git clone --recurse-submodules https://github.com/TotalKrill/.nvim.git ~/.config/nvim
+
+echo "Use pluginstall to compile ycm and download plugins"
+neovim -c ":PlugInstall" -c ":qa"
+
+echo "if no errors, you can now run neovim with 'nvim'"
